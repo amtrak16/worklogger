f14282e6f136d2f8d697377b113ba40e1c8bdb0f
Tony Lauf <LAU5619@s258769.nml.com>
2018-03-06 11:20:57 -0600

    corrected layout issues

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 import './ui-toolkit/css/nm-cx/main.css'
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.state = 
+    this.state =
       {
         projWork: [],
         projPers: [],
@@ -19,7 +19,7 @@ class App extends Component {
         descMsg: '',
         minsErr: false,
         minsMsg: '',
-      } 
+      }
 
     this.inputReq = 'Input Required!'
     this.integerReq = 'Enter a number for minutes.'
@@ -29,40 +29,40 @@ class App extends Component {
     this.onMinsIn = this.onMinsIn.bind(this)
     this.onAddClick = this.onAddClick.bind(this)
 
-    }
+  }
 
-  onProjIn (evt) {
+  onProjIn(evt) {
     if (evt.target.value.length < 0) {
       this.setState({ projErr: true, projMsg: this.inputReq })
     } else {
-      this.setState({ projVal: evt.target.value, projErr: false, projMsg: ''})
+      this.setState({ projVal: evt.target.value, projErr: false, projMsg: '' })
     }
   }
 
-  onDescIn (evt) {
+  onDescIn(evt) {
     if (evt.target.value.length < 0) {
       this.setState({ descErr: true, descMsg: this.inputReq })
     } else {
-      this.setState({ descVal: evt.target.value, descErr: false, descMsg: ''})
+      this.setState({ descVal: evt.target.value, descErr: false, descMsg: '' })
     }
   }
 
-  onMinsIn (evt) {
-    if (!Number.isInteger(parseInt(evt.target.value,10))) {
+  onMinsIn(evt) {
+    if (!Number.isInteger(parseInt(evt.target.value, 10))) {
       this.setState({ minsErr: true, minsMsg: this.integerReq })
     } else {
-      this.setState({ minsVal: evt.target.value, minsErr: false, minsMsg: ''})
+      this.setState({ minsVal: evt.target.value, minsErr: false, minsMsg: '' })
     }
   }
 
-  onAddClick (evt) {
+  onAddClick(evt) {
     evt.preventDefault()
     const newWorkLogItem = {
       projVal: this.state.projVal,
       descVal: this.state.descVal,
       minsVal: this.state.minsVal
     }
-    if (this.state.projVal === "Work"){
+    if (this.state.projVal === "Work") {
       let arr = this.state.projWork
       arr.push(newWorkLogItem)
       this.setState({ projWork: arr, projVal: '', descVal: '', minsVal: '' })
@@ -72,7 +72,7 @@ class App extends Component {
       this.setState({ projPers: arr, projVal: '', descVal: '', minsVal: '' })
     }
   }
-  
+
   render() {
     return (
       <div className="App">
@@ -94,14 +94,14 @@ class App extends Component {
             </div>
             <div class="row">
               <div class="small-4 columns md-text-field with-floating-label">
-                <input type="text" id="desc_in" value={this.state.descVal} required onChange={this.onDescIn}/>
+                <input type="text" id="desc_in" value={this.state.descVal} required onChange={this.onDescIn} />
                 <label for="desc_in">Description</label>
                 <span class="error">{this.state.descMsg}</span>
               </div>
             </div>
             <div class="row">
               <div class="small-4 columns md-text-field with-floating-label">
-                <input type="number" id="mins_in" max="480" min="15" value={this.state.minsVal} step="15"required onChange={this.onMinsIn}/>
+                <input type="number" id="mins_in" max="480" min="15" value={this.state.minsVal} step="15" required onChange={this.onMinsIn} />
                 <label for="mins_in">Minutes in multiple of 15</label>
                 <span class="error">{this.state.minMsg}</span>
               </div>
@@ -109,9 +109,15 @@ class App extends Component {
             <div class="row padding-small">
               <button class="button btn-cta" >Add Item</button>
             </div>
-          </form> 
-            <RenderWorkItems workItemList={this.state.projWork}/>
-            <RenderWorkItems workItemList={this.state.projPers}/>
+          </form>
+          <div class="row">
+            <div class="small-6 columns">
+              <RenderWorkItems workItemList={this.state.projWork} type="Work"/>
+            </div>
+            <div class="small-6 columns">
+              <RenderWorkItems workItemList={this.state.projPers} type="Personal"/>
+            </div>
+          </div>
         </div>
       </div>
     );
@@ -122,19 +128,33 @@ class RenderWorkItems extends Component {
   render() {
     let workItemRows = [];
     this.props.workItemList.forEach((workItem, idx) => {
+      if (idx === 0) {
+        workItemRows.push(
+          <div class="row">
+            <div class="small-3 columns"><h1>{this.props.type}</h1></div>
+            <div class="small-3 columns">&nbsp;</div>         
+          </div>       
+        )
+        workItemRows.push(
+        <div class="row">
+          <div class="small-3 columns">Description</div>
+          <div class="small-1 columns">Minutes</div>
+          <div class="small-2 columns">&nbsp;</div>
+        <div class="row"></div>
+      </div>
+        )
+      }
       workItemRows.push(
-      <div class="row">
+        <div class="row">
           <div class="small-3 columns">{workItem.descVal}</div>
           <div class="small-1 columns">{workItem.minsVal}</div>
-      </div>
-      )})
+          <div class="small-2 columns">&nbsp;</div>
+        </div>
+      )
+    })
 
     return (
-      <div class="row">
-        <div class="small-3 columns">Work</div>
-        <div class="small-1 columns">Minutes</div>
-        <div>{workItemRows}</div> 
-      </div>
+        <div>{workItemRows}</div>
     );
   }
 }
